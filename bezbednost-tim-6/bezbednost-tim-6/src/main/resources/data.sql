2401a1aa709201cceee587926245072db2f3fb5a
Kristina Andrijin <andrijinkristina@gmail.com>
2023-03-25 20:04:14 +0100

    MySql configurations

---
+++ bezbednost-tim-6/bezbednost-tim-6/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (activated, address, blocked, email, name, profile_picture, surname, password, telephone_number)
+VALUES (true, 'Narodnog fronta 5', false, 'andrijinkristina@gmail.com', 'Kristina', 'profile_picture5', 'Andrijin', '$2a$10$iYCFgw.vl7KlYVikqSbLtOyAR5lNEKalF29dfYfGn7SRiE8pbskeu', '6521545154');
