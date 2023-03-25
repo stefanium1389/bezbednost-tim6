cc58eac533eb3a505692961e20c2cefa02cd04b0
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-03-25 10:36:12 +0100

    Basic spring project

---
+++ bezbednost_projekat/src/main/java/ibtim6/BezbednostTim6Application.java
@@ -0,0 +1,13 @@
+package ibtim6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezbednostTim6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezbednostTim6Application.class, args);
+	}
+
+}
---
+++ bezbednost_projekat/src/test/java/ibtim6/BezbednostTim6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibtim6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezbednostTim6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
