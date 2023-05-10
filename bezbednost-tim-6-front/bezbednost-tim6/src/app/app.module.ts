c5e425a95af1754d25af856454e816b7525315fc
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-05-10 08:50:56 +0200

    Revert "pokusaj sms"
    This reverts commit cae80886c6fd3581a2a782f4c0710e5a0ca062f5.

--- bezbednost-tim-6-front/bezbednost-tim6/src/app/app.module.ts
+++ bezbednost-tim-6-front/bezbednost-tim6/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
