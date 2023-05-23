3c301243ab27301d4a96ce1c4c6a28f84accdc48
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-05-23 21:29:43 +0200

    Update error-interceptor.service.ts

--- bezbednost-tim-6-front/bezbednost-tim6/src/app/error-interceptor.service.ts
+++ bezbednost-tim-6-front/bezbednost-tim6/src/app/error-interceptor.service.ts
@@ -1,11 +1,10 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, retry, throwError } from 'rxjs';
+import { Observable, catchError, switchMap, tap, throwError } from 'rxjs';
 import { LoginService } from './backend-services/login.service';
 import { JwtService } from './jwt.service';
 import { LoginResponse } from './dtos/LoginDtos';
 import { Router } from '@angular/router';
-import { SuccessDTO } from './dtos/MessageDtos';
 
 @Injectable({
   providedIn: 'root'
@@ -16,22 +15,30 @@ export class ErrorInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
-        if(err.status == 403){
-          const dto:LoginResponse = {accessToken:this.jwtService.getAccessToken()!, refreshToken: this.jwtService.getRefreshToken()!}
-          this.loginService.refreshToken(dto).subscribe({
-            next: (result) => {
+        if (err.status == 403) {
+          const dto: LoginResponse = {
+            accessToken: this.jwtService.getAccessToken()!,
+            refreshToken: this.jwtService.getRefreshToken()!
+          };
+  
+          return this.loginService.refreshToken(dto).pipe(
+            switchMap((result) => {
               this.jwtService.setAccessToken(result.message);
-              next.handle(req).subscribe();
-            },
-            error: (error) => {
-              alert("Session expired, pleaselogin again")
-              //this.jwtService.logout();
-              //router.navigate(['']);
-            }
-          })
+              return next.handle(req);
+            }),
+            catchError((error) => {
+              if(error.status == 510){
+                alert("Session expired, please login again");
+                this.jwtService.logout();
+                this.router.navigate(['']);
+              }
+              return throwError(error);
+            })
+          );
         }
         return throwError(err);
       })
-    )
+    );
   }
+    
 }
