fefeedc631152d20566054c5c80dae3200a6bbe0
Kristina Andrijin <andrijinkristina@gmail.com>
2023-05-24 18:45:49 +0200

    feat: certificate revocation

--- bezbednost-tim-6/bezbednost-tim-6/src/main/java/bezbednosttim6/controller/CertificateController.java
+++ bezbednost-tim-6/bezbednost-tim-6/src/main/java/bezbednosttim6/controller/CertificateController.java
@@ -140,7 +140,7 @@ public class CertificateController {
 
 
 	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-	@PostMapping ("request/reject/{requestId}")
+	@PutMapping ("request/reject/{requestId}")
 	public ResponseEntity<?> rejectRequest (@PathVariable("requestId") Long requestId, @RequestBody ReasonDTO reasonDTO, Principal principal) {
 		try {
 			CertificateRequest newRequest = certificateRequestService.rejectRequest(requestId, reasonDTO.getReason(), principal);
@@ -153,7 +153,7 @@ public class CertificateController {
 	}
 
 	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-	@PostMapping ("request/accept/{requestId}")
+	@PutMapping ("request/accept/{requestId}")
 	public ResponseEntity<?> acceptRequest (@PathVariable("requestId") Long requestId, Principal principal) {
 		try {
 			CertificateRequest newRequest = certificateRequestService.acceptRequest(requestId, principal);
@@ -162,6 +162,19 @@ public class CertificateController {
 		{
 			ErrorDTO error = new ErrorDTO(e.getMessage());
 			return new ResponseEntity<ErrorDTO>(error,HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	@PutMapping ("revoke/{serialNumber}")
+	public ResponseEntity<?> revokeCertificate (@PathVariable("serialNumber") Long serialNumber, @RequestBody ReasonDTO reasonDTO, Principal principal) {
+		try {
+			certificateService.revokeCertificate(serialNumber, principal, reasonDTO.getReason());
+			return new ResponseEntity<>("Revoked successfully", HttpStatus.OK);
+		} catch (Exception e)
+		{
+			ErrorDTO error = new ErrorDTO(e.getMessage());
+			return new ResponseEntity<ErrorDTO>(error,HttpStatus.BAD_REQUEST);
 		}
 	}
 
--- bezbednost-tim-6/bezbednost-tim-6/src/main/resources/data.sql
+++ bezbednost-tim-6/bezbednost-tim-6/src/main/resources/data.sql
@@ -1,8 +1,6 @@
--- INSERT INTO users (email, password, name, surname, telephone_number, role_id, activated)
--- VALUES ('andrijinkristina@gmail.com', '$2a$10$iYCFgw.vl7KlYVikqSbLtOyAR5lNEKalF29dfYfGn7SRiE8pbskeu', 'Kristina','Andrijin', '6521545154', 1, true);
---
--- INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
--- INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO users (email, password, name, surname, telephone_number, role_id, activated)
+VALUES ('andrijinkristina@gmail.com', '$2a$10$iYCFgw.vl7KlYVikqSbLtOyAR5lNEKalF29dfYfGn7SRiE8pbskeu', 'Kristina','Andrijin', '6521545154', 1, true);
 
---insert into certificates (serial_number, certificate_type, issuer, signature_algorithm, status, valid_from, valid_to, user_id)
---values (1680527310325, "ROOT", 1, "SHA256withRSA", "VALID", "2023-04-03 14:59:27", "2027-05-12 14:59:27", 1);
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+
