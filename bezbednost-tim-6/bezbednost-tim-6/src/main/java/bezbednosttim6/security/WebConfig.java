9bd896b33f62e5d7175d0573fe06b3647a2fb12f
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-06-15 23:11:21 +0200

    https angular

--- bezbednost-tim-6/bezbednost-tim-6/src/main/java/bezbednosttim6/security/WebConfig.java
+++ bezbednost-tim-6/bezbednost-tim-6/src/main/java/bezbednosttim6/security/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
             .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("https://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
